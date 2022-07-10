be820b2aea95795376dd9bd1eaa176c4ee6aa533
duy-tnb <duy-tnb@genkisystem.com>
2022-07-10 19:14:45 +0900

    add router

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,22 +1,18 @@
 import React from "react";
 import Carousel from "../component/home/Carousel";
-import Footer from "../component/common/Footer";
-import Header from "../component/common/Header";
-import Product from "../component/Product/Product";
+import Product from "../component/product/Product";
 import Banner from "../component/home/Banner";
 import LatestNews from "../component/home/LatestNews";
 import Feature from "../component/home/Feature";
 
-export default function Home() {
+export default function HomePage() {
   return (
     <div>
-      <Header />
       <Carousel />
       <Product />
       <Banner />
       <Feature />
       <LatestNews />
-      <Footer />
     </div>
   );
 }
